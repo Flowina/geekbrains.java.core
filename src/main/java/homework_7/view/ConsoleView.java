a49d6b9e243bbb0b08c607729c833559420a88b6
Flowina <bilobik@mail.ru>
2021-10-03 16:32:09 +0300

    Homework 8. Save api response to DB

--- src/main/java/homework_7/view/ConsoleView.java
+++ src/main/java/homework_7/view/ConsoleView.java
@@ -2,12 +2,19 @@ package homework_7.view;
 
 import homework_7.controller.WeatherController;
 import homework_7.model.WeatherResponse;
+import homework_8.DataBaseRepository;
+import homework_8.entities.Weather;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConsoleView {
+    private final String CITY = "Санкт-Петербург";
+    DataBaseRepository repository = new DataBaseRepository();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private WeatherController controller;
 
@@ -17,37 +24,73 @@ public class ConsoleView {
 
     public void run() {
         String action = "";
+        String actionLevel1 = "";
         try {
             do {
-                System.out.println("1, 3, 5 дней");
-                System.out.println("0. Exit");
+                System.out.println("1 - читать из базы данных");
+                System.out.println("2 - api");
+                System.out.println("0 - Exit");
                 System.out.println("===========================================");
-                System.out.println("Введите количество дней");
+                System.out.println("Сделайте выбор");
                 System.out.println("===========================================");
-                action = br.readLine();
+                actionLevel1 = br.readLine();
                 System.out.println("\n");
 
-                switch(action)
-                {
-                    case "1":
-                    case "3":
-                    case "5":
-                        System.out.print("Загрузка...");
-                        WeatherResponse wr = controller.get(Integer.parseInt(action));
-                        System.out.print("\b\b\b\b\b\b\b\b\b\b\b");
-                        System.out.println(wetherResponseToString(wr));
-                        break;
-
+                switch (actionLevel1) {
                     case "0":
                         System.out.println("BYE!");
                         System.exit(0);
                         break;
+                    case "1":
+                        List<Weather> forecasts = repository.getSavedToDBWeather();
+                        if (forecasts.size() == 0) {
+                            System.out.println("Нет данных");
+                        } else {
+                            for (Weather forecast:
+                                    forecasts) {
+                                System.out.println(String.format("id=%s, city=%s, date=%s, weather=%s, temperature=%s%n",
+                                        forecast.getId().get(), forecast.getCity(), forecast.getLocalDate(), forecast.getWeatherText(),
+                                        forecast.getTemperature()));
+                            }
+                        }
+                        break;
+                    case "2":
+                        do {
+                            System.out.println("1, 3, 5 дней");
+                            System.out.println("0. Exit");
+                            System.out.println("===========================================");
+                            System.out.println("Введите количество дней");
+                            System.out.println("===========================================");
+                            action = br.readLine();
+                            System.out.println("\n");
 
-                    default:
-                        System.out.println("Invalid Action! Please enter again");
+                            switch (action) {
+                                case "1":
+                                case "3":
+                                case "5":
+                                    System.out.print("Загрузка...");
+                                    WeatherResponse wr = controller.get(Integer.parseInt(action));
+                                    System.out.print("\b\b\b\b\b\b\b\b\b\b\b");
+                                    System.out.println(wetherResponseToString(wr));
+
+                                    saveToDB(wr);
+                                    break;
+
+                                case "0":
+                                    System.out.println("BYE!");
+                                    System.exit(0);
+                                    break;
+
+                                default:
+                                    System.out.println("Invalid Action! Please enter again");
+                                    break;
+                            }
+                        } while (!action.equals("0"));
                         break;
                 }
-            } while (!action.equals("0"));
+            }
+            while (!action.equals("0") || !actionLevel1.equals("0"));
+
             br.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -55,6 +98,30 @@ public class ConsoleView {
             e.printStackTrace();
         }
     }
+
+    private void saveToDB(WeatherResponse wr) {
+        List<Weather> list = new ArrayList<>();
+        for (WeatherResponse.Forecast f :
+                wr.forecasts) {
+            if (f.hours.length == 0) {
+                continue;
+            }
+            WeatherResponse.Hour h = f.hours[f.hours.length / 2];
+            Weather weatherItem = new Weather(
+                    CITY,
+                    f.date,
+                    precTypeToString(h.prec_type),
+                    (double) h.temp
+            );
+            list.add(weatherItem);
+        }
+        try {
+            repository.saveWeatherToDataBase(list);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public String precTypeToString(int prec_type) {
         switch (prec_type) {
             case 0:
@@ -74,14 +141,14 @@ public class ConsoleView {
 
     public String wetherResponseToString(WeatherResponse wr) {
         StringBuilder sb = new StringBuilder();
-        for (WeatherResponse.Forecast f:
+        for (WeatherResponse.Forecast f :
                 wr.forecasts) {
             if (f.hours.length == 0) {
                 continue;
             }
-            WeatherResponse.Hour h =  f.hours[f.hours.length / 2];
+            WeatherResponse.Hour h = f.hours[f.hours.length / 2];
             sb.append(String.format("В городе %s на дату %s погода ожидается %s, температура  %s%n",
-                    "Санкт-Петербург", f.date,precTypeToString(h.prec_type), h.temp) );
+                    "Санкт-Петербург", f.date, precTypeToString(h.prec_type), h.temp));
         }
         return sb.toString();
     }
--- src/main/java/homework_8/Main.java
+++ src/main/java/homework_8/Main.java
@@ -1,32 +1,14 @@
 package homework_8;
 
-import homework_8.entities.Weather;
+import homework_7.controller.WeatherController;
+import homework_7.model.YandexWeather;
+import homework_7.view.ConsoleView;
 
-import java.sql.SQLException;
-import java.util.List;
-
-/*1. Добавить поддержку SQLite в проект.
-2. Создать класс-репозиторий, отвечающий за взаимодействие с базой данных.
-3. Организовать запись данных в базу при каждом успешном API запросе.
- Формат - String city, String localDate, String weatherText, Double temperature.
-4. Организовать чтение из базы всех данных по пункту меню (требует переработки меню)
-5. Учесть, что соединение всегда нужно закрывать*/
 public class Main {
     public static void main(String[] args) {
-        DataBaseRepository rep = new DataBaseRepository();
-        try {
-            //rep.saveWeatherToDataBase(new Weather("спб","2021-10-01","солнечно",1.0));
+        WeatherController controller = new WeatherController(new YandexWeather());
+        ConsoleView view = new ConsoleView(controller);
 
-            List<Weather> forecasts = rep.getSavedToDBWeather();
-            for (Weather forecast:
-                 forecasts) {
-                System.out.println(String.format("id=%s, city=%s, date=%s, weather=%s, temperature=%s%n",
-                        forecast.getId().get(), forecast.getCity(), forecast.getLocalDate(), forecast.getWeatherText(),
-                        forecast.getTemperature()));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        view.run();
     }
 }
